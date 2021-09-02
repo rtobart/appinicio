3f7c3cf8002ab4887013b2bf6b82d9fe5c74902f
rtobart <robinson.tobar.ti@gmail.com>
2021-09-02 14:30:38 -0400

    menu a mano

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {IonicModule} from '@ionic/angular';
 import {EncabezadoComponent} from './encabezado/encabezado.component'
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [EncabezadoComponent],
-  exports: [EncabezadoComponent],
+  declarations: [EncabezadoComponent,
+    FooterComponent],
+  exports: [EncabezadoComponent, 
+    FooterComponent],
   imports: [
     CommonModule,
     IonicModule
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,3 +1,4 @@
+import { ComponentsModule } from 'src/app/components/components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ComponentsModule
   ],
   declarations: [HomePage]
 })
